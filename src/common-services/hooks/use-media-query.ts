6b7b7f2332f3c307ff0fedee26d99b272202526d
Vivek Menon <vivekpmenon@gmail.com>
2024-11-13 22:47:02 +0000

    creatign media query hook for responsiveness

---
+++ src/common-services/hooks/use-media-query.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    const handleChange = () => setMatches(mediaQuery.matches);
+
+    setMatches(mediaQuery.matches);
+    mediaQuery.addListener(handleChange);
+
+    return () => {
+      mediaQuery.removeListener(handleChange);
+    };
+  }, [query]);
+
+  return matches;
+}
