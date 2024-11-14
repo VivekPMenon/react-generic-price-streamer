f93b5295287f815b42db210eb8242bb711d00243
sandheepsoman <sandheepsoman@gmail.com>
2024-11-14 17:10:04 +0530

    adding search feature for trace widget

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -9,43 +9,9 @@ import HighchartsReact from 'highcharts-react-official';
 import darkTheme from '../../common-components/charts/dark-theme';
 import { MarketLandscape } from '../market-landscape';
 
-Highcharts.setOptions(darkTheme);
+
 
 export function Dashboard() {
-
-  const options = {
-    chart: {
-      type: 'spline'
-    },
-    title: {
-      text: ''
-    },
-    xAxis: {
-      title: {
-        text: 'Time Period'
-      },
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'] // Example time labels
-    },
-    yAxis: {
-      title: {
-        text: 'Price (USD)'
-      }
-    },
-    series: [
-      {
-        name: 'MSFT',
-        data: [305, 310, 298, 320, 330, 315] // Random fluctuating data for demonstration
-      },
-      {
-        name: 'AAPL',
-        data: [150, 160, 155, 170, 165, 158] // Random fluctuating data for demonstration
-      },
-      {
-        name: 'ATT',
-        data: [28, 32, 27, 30, 29, 33] // Random fluctuating data for demonstration
-      }
-    ]
-  };
 
   return (
     <div className="multi-widget-dashboard ">
