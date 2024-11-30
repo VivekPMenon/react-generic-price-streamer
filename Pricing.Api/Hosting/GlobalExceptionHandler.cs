6181bdd40da864007c86446ed7de232779d73190
vishyiscool <vishyiscool@gmail.com>
2024-11-30 08:04:26 +0000

    adding common utilities for Hosting and other MW

---
+++ Pricing.Api/Hosting/GlobalExceptionHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using Hosting.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace Hosting
+{
+    public sealed class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "An unexpected error occurred: {Message}", exception.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "An unexpected error occurred.",
+                Detail = exception.Message
+            };
+
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            httpContext.Response.ContentType = "application/json";
+
+            // Convert ProblemDetails to a JSON string
+            string jsonString = JsonSerializer.Serialize(problemDetails);
+
+            await httpContext.Response.WriteAsync(jsonString, cancellationToken);
+
+            return true;
+        }
+    }
+}
