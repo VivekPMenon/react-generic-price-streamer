8688e15927efb37a240fe5cd3f1d32fc68223d6b
Vivek Menon <vivekpmenon@gmail.com>
2025-01-18 11:16:59 +0000

    more on transiental ui

--- TransientAI.UI/transientai-ui/src/components/axes/todays-axes.tsx
+++ TransientAI.UI/transientai-ui/src/components/axes/todays-axes.tsx
@@ -66,12 +66,54 @@ export function TodaysAxes() {
 
       <div className={styles['clients-and-holdings']}>
         <div>
-          <div className='sub-header'>Clients</div>
+          <div className='sub-header'>Top Clients</div>
 
-          <DataGrid isSummaryGrid={true}
-            rowData={rowData}
-            columnDefs={columnDefs}>
-          </DataGrid>
+          <div className='news'>
+            <div className='news-item'>
+              <div className='news-content'>
+                <div className='news-title'>
+                  VANGUARD
+                </div>
+                <div className='news-description'>
+                  Here is some information about VANGUARD to give some insight/quick info
+                </div>
+              </div>
+
+              <div className='news-menu'>
+                <i className='fa-solid fa-ellipsis-v fa-lg'></i>
+              </div>
+            </div>
+
+            <div className='news-item'>
+              <div className='news-content'>
+                <div className='news-title'>
+                  Goldman Sachs
+                </div>
+                <div className='news-description'>
+                  Here is some information about Goldman Sachs to give some insight/quick info
+                </div>
+              </div>
+
+              <div className='news-menu'>
+                <i className='fa-solid fa-ellipsis-v fa-lg'></i>
+              </div>
+            </div>
+
+            <div className='news-item'>
+              <div className='news-content'>
+                <div className='news-title'>
+                  Capital
+                </div>
+                <div className='news-description'>
+                  Here is some information about Capital to give some insight/quick info
+                </div>
+              </div>
+
+              <div className='news-menu'>
+                <i className='fa-solid fa-ellipsis-v fa-lg'></i>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div>
