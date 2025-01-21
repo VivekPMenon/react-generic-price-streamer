15f020fd730db514103e47a6ea57d02f356a327b
Vivek Menon <vivekpmenon@gmail.com>
2025-01-21 12:43:22 +0000

    more on transiental ui

--- TransientAI.UI/transientai-ui/src/components/market-data/price-graph.tsx
+++ TransientAI.UI/transientai-ui/src/components/market-data/price-graph.tsx
@@ -5,7 +5,8 @@ import dynamic from 'next/dynamic';
 // Dynamically import HighchartsReact with SSR disabled
 const HighchartsReact = dynamic(() => import('highcharts-react-official'), { ssr: false });
 import Highstock from 'highcharts/highstock';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { GraphDataPoint, marketDataService } from '@/services/market-data';
 
 export interface PriceGraphProps {
   onExpandCollapse: (state: boolean) => void;
@@ -13,101 +14,139 @@ export interface PriceGraphProps {
 
 export function PriceGraph(props: PriceGraphProps) {
 
+  const bondName = 'INTC 4.15 08/05/32';
+
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const [graphDataPoints, setGraphDataPoints] = useState<GraphDataPoint[]>([]);
 
-  const chartOptions: Highcharts.Options = {
-    chart: {
-      type: 'area',
-      backgroundColor: '#0C101B',
-    },
-    title: {
-      text: '',
-    },
-    xAxis: {
-      type: 'datetime',
-      labels: { style: { color: '#dddddd' } },
-      gridLineWidth: 0
-    },
-    yAxis: {
-      title: { text: null },
-      labels: { style: { color: '#dddddd' } },
-      gridLineWidth: 0
-    },
-    navigator: {
-      enabled: false
-    },
-    scrollbar: {
-      enabled: false
-    },
-    rangeSelector: {
-      enabled: true,
-      inputEnabled: false,
-      buttonTheme: {
-        fill: '#1E2128',
-        stroke: '#1E2128',
-        padding: 7,
-        style: {
-          color: '#FFFFFF',
-          borderRadius: 5
-        },
-        states: {
-          hover: {
-            fill: '#555555', // Background color on hover
-            style: {
-              color: '#FFFFFF', // Text color on hover
-            },
-          },
-          select: {
-            fill: 'white', // Background color when selected
-            style: {
-              color: 'black', // Text color when selected
-            },
-          },
-        },
-      },
-      inputStyle: {
-        color: '#FFFFFF', // Input text color
-        backgroundColor: '#333333', // Input background color
-      },
-      labelStyle: {
-        color: '#FFFFFF'
-      },
-    },
-    plotOptions: {
-      area: {
-        fillColor: {
-          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-          stops: [
-            [0, 'rgba(0, 255, 0, 0.2)'], // Lighter at the top
-            [1, 'rgba(0, 0, 0, 0)'],
-          ],
-        },
-        lineColor: '#198754',
-        lineWidth: 1,
-        marker: { enabled: true, radius: 3 },
-        threshold: null,
-      },
-    },
-    series: [
-      {
+  const chartOptions = useMemo<Highcharts.Options>(() => getChartOptions(), [graphDataPoints])
+
+  useEffect(() => loadGraphDataPoints(), []);
+
+  function loadGraphDataPoints() {
+    const loadGraphDataPointsAsync = async () => {
+      const result = await marketDataService.getMarketData('1M', bondName);
+      setGraphDataPoints(result);
+    };
+
+    loadGraphDataPointsAsync();
+  }
+
+  // todo.. move highchart options to a common component so theming can be controlled
+  // todo. migrate theming from TS to CSS
+  function getChartOptions() {
+    
+    let seriesData: any[] = [];
+    if (graphDataPoints) {
+      seriesData = graphDataPoints.map(graphDataPoint => {
+        const date = new Date(graphDataPoint.date!);
+        const timestamp = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+
+        return [timestamp, graphDataPoint.mid_yield]
+      });
+    }
+
+    const chartOptions: Highcharts.Options = {
+      chart: {
         type: 'area',
-        name: 'Data',
-        data: [
-          [Date.UTC(2023, 0, 1, 8, 0), 152.30], // 8:00 AM
-          [Date.UTC(2023, 0, 1, 9, 0), 153.10], // 9:00 AM
-          [Date.UTC(2023, 0, 1, 10, 0), 152.95], // 10:00 AM
-          [Date.UTC(2023, 0, 1, 11, 0), 153.50], // 11:00 AM
-          [Date.UTC(2023, 0, 1, 12, 0), 153.20], // 12:00 PM
-          [Date.UTC(2023, 0, 1, 13, 0), 153.00], // 1:00 PM
-
-          [Date.UTC(2023, 0, 1, 14, 0), null],   // 2:00 PM (No data)
-          [Date.UTC(2023, 0, 1, 15, 0), null],   // 3:00 PM (No data)
-          [Date.UTC(2023, 0, 1, 16, 0), null],   // 4:00 PM (No data)
-        ],
+        backgroundColor: '#0C101B',
       },
-    ],
-  };
-  
+      title: {
+        text: '',
+      },
+      xAxis: {
+        type: 'datetime',
+        labels: { style: { color: '#dddddd' } },
+        gridLineWidth: 0
+      },
+      yAxis: {
+        title: { text: null },
+        labels: { style: { color: '#dddddd' } },
+        gridLineWidth: 0
+      },
+      navigator: {
+        enabled: false
+      },
+      scrollbar: {
+        enabled: false
+      },
+      rangeSelector: {
+        enabled: true,
+        inputEnabled: false,
+        buttons: [
+          {
+            type: 'month',
+            count: 1,
+            text: '1M', // Label for the button
+          },
+          {
+            type: 'month',
+            count: 3,
+            text: '3M', // Label for the button
+          },
+          {
+            type: 'all',
+            text: 'All', // Label for the button
+          }
+        ],
+        buttonTheme: {
+          fill: '#1E2128',
+          stroke: '#1E2128',
+          padding: 7,
+          style: {
+            color: '#FFFFFF',
+            borderRadius: 5
+          },
+          states: {
+            hover: {
+              fill: '#555555', // Background color on hover
+              style: {
+                color: '#FFFFFF', // Text color on hover
+              },
+            },
+            select: {
+              fill: 'white', // Background color when selected
+              style: {
+                color: 'black', // Text color when selected
+              },
+            },
+          },
+        },
+        inputStyle: {
+          color: '#FFFFFF', // Input text color
+          backgroundColor: '#333333', // Input background color
+        },
+        labelStyle: {
+          color: '#FFFFFF'
+        },
+      },
+      plotOptions: {
+        area: {
+          fillColor: {
+            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+            stops: [
+              [0, 'rgba(0, 255, 0, 0.2)'], // Lighter at the top
+              [1, 'rgba(0, 0, 0, 0)'],
+            ],
+          },
+          lineColor: '#198754',
+          lineWidth: 1,
+          marker: { enabled: true, radius: 3 },
+          threshold: null,
+        },
+      },
+      series: [
+        {
+          type: 'area',
+          name: 'Data',
+          data: seriesData
+        },
+      ],
+    };
+
+    return chartOptions;
+  }
+
   function expandOrCollapsePanel() {
     setIsExpanded(!isExpanded);
     props.onExpandCollapse(!isExpanded);
@@ -115,7 +154,7 @@ export function PriceGraph(props: PriceGraphProps) {
 
   return (
     <div className={`${styles['price-graph']} widget`}>
-      
+
       <div className='widget-title'>
         Details
         {/* <i className='fa-solid fa-expand toggler' onClick={() => expandOrCollapsePanel()}></i> */}
@@ -123,7 +162,7 @@ export function PriceGraph(props: PriceGraphProps) {
 
       <div className={`${styles['price-graph-title']}`}>
         <i className='fa-solid fa-ban'></i>
-        BOEING (B.A) 5.875% 2040
+        {bondName}
 
         <div className='pill gray padded ml-auto rounded'>
           <i className='fa-solid fa-chart-simple'></i>
