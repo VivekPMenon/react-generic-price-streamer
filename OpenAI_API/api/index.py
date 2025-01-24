28cbb056c5da6064c0cbf9089d37db523475e2a0
Vivek Menon <vivekpmenon@gmail.com>
2025-01-24 21:58:57 +0000

    Python web api

--- OpenAI_API/api/index.py
+++ OpenAI_API/api/index.py
@@ -14,14 +14,29 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
-cache = Cache(app, config={"CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": 28800})  # 8 hours
+cache = Cache(app, config={"CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": 1})  # 8 hours
 CORS(app)
 
 @app.route('/api/news', methods=['GET'])
 @cache.cached()
 def get_news():
   #fetch_news_with_token_usage()
-  return jsonify(fetch_news_with_token_usage())
+  from phi.tools.yfinance import YFinanceTools
+
+    # Initialize the YFinanceTools
+  yfinance_tools = YFinanceTools(
+        stock_price=False,
+        analyst_recommendations=False,
+        company_info=False,
+        company_news=True
+    )
+    
+  try:
+        # Replace with a test query
+    result = yfinance_tools.get_company_news(symbol="AAPL", num_stories=1)
+    return {"status": "success", "data": result}
+  except Exception as e:
+    return {"status": "failure", "error": str(e)}
 
 # Expose the Flask app for Vercel
 app = app
