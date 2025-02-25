32a4de40731535ea4302866d59a694f85b860b9b
viswanath <vishyiscool@gmail.com>
2025-02-25 11:11:00 +0000

    Update string-operations.ts

--- Pricing.UI/src/common-services/utility-functions/string-operations.ts
+++ Pricing.UI/src/common-services/utility-functions/string-operations.ts
@@ -1,17 +1,101 @@
-export function generateRandomString(len: number) {
-  return Array.apply(0, Array(len))
-    .map(function () {
-      return (function (charset) {
-        return charset.charAt(
-          Math.floor(Math.random() * charset.length)
-        );
-      })(
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-      );
-    })
-    .join('');
-}
+[
+  {
+    "catiName": "CSFBconnect dot com VMO Infrastructure and DR",
+    "itOwner": "Ashok Sethumadhavan",
+    "businessOwner": "Amit Bhat",
+    "uploadedBy": "Mark Mendoza",
+    "date": "01.01.2025"
+  },
+  {
+    "catiName": "NTPA LON Clearance",
+    "itOwner": "Remington Monroe",
+    "businessOwner": "Trinity Stokes",
+    "uploadedBy": "Mylie McClure",
+    "date": "01.03.2025"
+  },
+  {
+    "catiName": "NTPA New York Production - Broker Symbol Xref",
+    "itOwner": "Eli Hill",
+    "businessOwner": "Reuben Riley",
+    "uploadedBy": "Riley Ibarra",
+    "date": "01.03.2025"
+  },
+  {
+    "catiName": "APAC EQ TESA Ticket Engine Storage App",
+    "itOwner": "Willow McGuire",
+    "businessOwner": "Layne George",
+    "uploadedBy": "Layne George",
+    "date": "01.05.2025"
+  },
+  {
+    "catiName": "MAS Electronic Payment System (MEPs+)",
+    "itOwner": "Davis Ware",
+    "businessOwner": "Isla Rodgers",
+    "uploadedBy": "Emma Keith",
+    "date": "01.11.2025"
+  },
+  {
+    "catiName": "Online Trader Position Details Reporting System",
+    "itOwner": "Andre Weiss",
+    "businessOwner": "Pablo White",
+    "uploadedBy": "Deanna Perry",
+    "date": "01.13.2025"
+  },
+  {
+    "catiName": "D1 Index Trading Pricing Tool",
+    "itOwner": "Gaige May",
+    "businessOwner": "Jaylah Gilbert",
+    "uploadedBy": "Maia Graves",
+    "date": "01.13.2025"
+  },
+  {
+    "catiName": "Japan Equities SmartBridge",
+    "itOwner": "Averi Bowman",
+    "businessOwner": "Trinity Herrera",
+    "uploadedBy": "Jamir Pierce",
+    "date": "01.14.2025"
+  },
+  {
+    "catiName": "NTPA - Real Time Trade Matching for GSCC securities",
+    "itOwner": "Amiyah Welz",
+    "businessOwner": "Eliana Miller",
+    "uploadedBy": "Marshall Cooper",
+    "date": "01.15.2025"
+  },
+  {
+    "catiName": "SIX Repo Trading and Reference Data GUI",
+    "itOwner": "Jocelynn Higgins",
+    "businessOwner": "Corey Bender",
+    "uploadedBy": "Corey Bender",
+    "date": "01.19.2025"
+  },
+  {
+    "catiName": "NTPA London - Trading Account Master (TAQ)",
+    "itOwner": "Gaige Roth",
+    "businessOwner": "Nathalia Conway",
+    "uploadedBy": "Dashawn Stevens",
+    "date": "01.21.2025"
+  },
+  {
+    "catiName": "Statement Delivery Tool (SDT)",
+    "itOwner": "Javion Calderon",
+    "businessOwner": "Deanna Holmes",
+    "uploadedBy": "N/A",
+    "date": "01.21.2025"
+  },
+  {
+    "catiName": "NTPA London Production - Customer Master (CA)",
+    "itOwner": "Nevaeh Mason",
+    "businessOwner": "Nathalia Dorsey",
+    "uploadedBy": "Cassandra Fox",
+    "date": "01.22.2025"
+  }
+]
 
-export function capitalize(str: string) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+interface CatiData {
+  catiName: string;
+  itOwner: string;
+  businessOwner: string;
+  uploadedBy: string;
+  date: string; // Format: "MM.DD.YYYY"
 }
