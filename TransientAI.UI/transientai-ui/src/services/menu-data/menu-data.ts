e331bf95636c0c69aed3dedaa8baecffb908f713
Guna-Ramesh <gunasekaran.r@innoboon.com>
2025-03-11 11:57:34 +0530

    menu changes

--- TransientAI.UI/transientai-ui/src/services/menu-data/menu-data.ts
+++ TransientAI.UI/transientai-ui/src/services/menu-data/menu-data.ts
@@ -1,6 +1,43 @@
 import { MenuInfo } from "./model";
 
 export const menuInfoList: MenuInfo[] = [
+  // {
+  //   description: 'Today',
+  //   badgeCount: 5,
+  //   icon: 'fa-regular fa-snowflake'
+  // },
+  // {
+  //   description: `Today's Axes`,
+  //   badgeCount: 4,
+  //   icon: 'fa-solid fa-book-open',
+  //   // children: [
+  //   //   {description: 'Active Orders'},
+  //   //   {description: 'Recent Trades'},
+  //   //   {description: 'Call Notes'},
+  //   //   {description: 'Position Changes', subDescription: '11/24 9:45am'}
+  //   // ]
+  // },
+  // {
+  //   description: 'Trading Activity',
+  //   badgeCount: 3,
+  //   icon: 'fa-solid fa-ranking-star'
+  // },
+  // {
+  //   description: 'Market Data',
+  //   badgeCount: 4,
+  //   icon: 'fa-solid fa-chart-simple',
+  //   // children: [
+  //   //   {description: 'Breaking News', subDescription: '11/24 7:45am'},
+  //   //   {description: 'Earnings Updates'},
+  //   //   {description: 'Rating Changes'},
+  //   //   {description: 'Research Notes', subDescription: '11/24 8:45am'}
+  //   // ]
+  // },
+  // {
+  //   description: 'Client Data',
+  //   badgeCount: 3,
+  //   icon: 'fa-solid fa-user'
+  // },
   {
     id: 'research-reports',
     route: '/dashboard/research-reports',
@@ -8,6 +45,12 @@ export const menuInfoList: MenuInfo[] = [
     badgeCount: 1,
     icon: 'fa-solid fa-book'
   },
+  // {
+  //   route: '/dashboard/risk-reports',
+  //   description: 'Risk Report Generation',
+  //   badgeCount: 1,
+  //   icon: 'fa-solid fa-bolt'
+  // },
   {
     id: 'risk-reports',
     route: '/dashboard/risk-report-portal',
@@ -38,9 +81,15 @@ export const menuInfoList: MenuInfo[] = [
   },
   {
     id: 'risk-metrics',
-    description: 'Risk Metrics',
+    description: 'Risk Insights',
     badgeCount: 1,
     icon: 'fa-solid fa-chart-column',
     route: '/dashboard/risk-metrics',
-  }
+  },
+  // {
+  //   description: 'Term Sheets',
+  //   badgeCount: 5,
+  //   icon: 'fa-solid fa-newspaper',
+  //   route: '/dashboard/term-sheets',
+  // },
 ];
