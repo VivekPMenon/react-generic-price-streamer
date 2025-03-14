ebd2566f845022330b0eff5f93216a38b95a8731
Vivek Menon <vivekpmenon@gmail.com>
2025-03-14 14:42:57 +0100

    Update endpoint-finder-service.ts

--- TransientAI.UI/transientai-ui/src/services/web-api-handler/endpoint-finder-service.ts
+++ TransientAI.UI/transientai-ui/src/services/web-api-handler/endpoint-finder-service.ts
@@ -6,6 +6,10 @@ class EndpointFinderService {
 
   // host name based look may not scale, will ned to resolve this from nextjs server side using environment variables
   getCurrentEnvInfo(): EndpointConfig {
+    if(typeof window === 'undefined') {
+      return endpointConfigs['localhost'];
+    }
+
     const currentHost = window.location.hostname;
     const config = endpointConfigs[currentHost];
 
