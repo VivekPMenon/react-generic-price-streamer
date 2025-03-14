67ae1e8b874b1263fa3f7a35a591192f3942b062
Vivek Menon <vivekpmenon@gmail.com>
2025-03-14 07:50:15 +0100

    risk metrics mobile view

--- TransientAI.UI/transientai-ui/src/components/risk-metrics/risk-metrics.tsx
+++ TransientAI.UI/transientai-ui/src/components/risk-metrics/risk-metrics.tsx
@@ -2,29 +2,32 @@ import { useMemo } from "react";
 import { DataGrid, getCurrencyColDefTemplate } from "../data-grid";
 import { ColDef } from "ag-grid-community";
 import { useRiskDataStore } from "@/services/risk-data/risk-data-store";
+import { useDeviceType } from "@/lib/hooks";
 
 export function RiskMetrics() {
 
+  const deviceType = useDeviceType();
   const columnDefs = useMemo<ColDef[]>(() => getColumnDef(), []);
   const { riskMetricsItems } = useRiskDataStore();  
 
   function getColumnDef(): ColDef[] {
+    const columnWidth = deviceType === 'mobile' ? 130 : 200;
     return [
       {
         field: 'name',
         headerName: 'Manager',
-        width: 200,
+        width: columnWidth,
         cellClass: 'fs-15'
       },
       {
         field: 'entity',
         headerName: 'Entity',
-        width: 200
+        width: columnWidth
       },
       {
         field: 'margin_excess',
         headerName: 'GS Margin Excess',
-        width: 200,
+        width: columnWidth,
         ...getCurrencyColDefTemplate()
       }
     ];
