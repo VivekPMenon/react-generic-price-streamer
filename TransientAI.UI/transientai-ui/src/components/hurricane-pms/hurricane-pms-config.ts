6754e1f81eaa12be92c6a51139f8c6a5a43739cd
Guna-Ramesh <gunasekaran.r@innoboon.com>
2025-04-07 18:27:57 +0530

    hurricane-pms: table design

--- TransientAI.UI/transientai-ui/src/components/hurricane-pms/hurricane-pms-config.ts
+++ TransientAI.UI/transientai-ui/src/components/hurricane-pms/hurricane-pms-config.ts
@@ -13,11 +13,226 @@ import {
 
 export const columnDefs: ColDef[] = [
     {
-        field: 'manager',
+        field: 'name',
         headerName: 'Portfolio',
         headerClass: `${styles['table-header']} ag-right-aligned-header`,
-        valueFormatter: (params: ValueFormatterParams) => '',
+        aggFunc: 'sum',
+        pinned: true,
+    },
+    {
+        field: 'pl',
+        headerName: 'P&L',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'delta_adj_gross',
+        headerName: 'Delta Adj Gross',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'gmv_usage_percent',
+        headerName: 'GMV Usage',
+        width: 85,
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'delta_adj',
+        headerName: 'Delta Adj',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'total_assets',
+        headerName: 'Total Assets',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        aggFunc: 'sum'
+    },
+]
+
+export const managerDetailsColDefs: ColDef[] = [
+    {
+        field: 'name',
+        headerName: 'Manager',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'strategy',
+        headerName: 'Strategy',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'order_side_1',
+        headerName: 'Order Side',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'security_type',
+        headerName: 'Security type',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'country_group',
+        headerName: 'Country Group',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'ticker',
+        headerName: 'Ticker',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'security_description',
+        headerName: 'security Description',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'quantity',
+        headerName: 'Quantity',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'status',
+        headerName: 'Price Status',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'trade_date',
+        headerName: 'Trade Date',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'settlement_date',
+        headerName: 'Settlement Date',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'executing_broker',
+        headerName: 'Execution Broker',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'clearing_broker',
+        headerName: 'Cleaning Broker',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'currency',
+        headerName: 'Security Currency',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
         width: 85,
         aggFunc: 'sum'
     },
 ]
+
+export const profitColDefs: ColDef[] = [
+    {
+        field: 'portfolio_manager',
+        headerName: 'Profolio Manager',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'security',
+        headerName: 'Security',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'last_price',
+        headerName: 'Last Price',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'pl',
+        headerName: 'P&L',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'pl_bps',
+        headerName: 'PLBPs',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+
+]
+
+export const lossColDefs: ColDef[] = [
+    {
+        field: 'portfolio_manager',
+        headerName: 'Profolio Manager',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'manager',
+        headerName: 'Security',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'last_price',
+        headerName: 'Last Price',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'pl',
+        headerName: 'P&L',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        width: 85,
+        aggFunc: 'sum'
+    },
+    {
+        field: 'pl_bps',
+        headerName: 'PLBPs',
+        headerClass: `${styles['table-header']} ag-right-aligned-header`,
+        aggFunc: 'sum'
+    },
+
+]
+
+export const defaultGridOptions: GridOptions = {
+    getRowId: (params: GetRowIdParams) => String(params.data),
+    autoSizeStrategy: {
+        type: 'fitCellContents',
+    },
+};
--- TransientAI.UI/transientai-ui/src/components/hurricane-pms/hurricane-pms.tsx
+++ TransientAI.UI/transientai-ui/src/components/hurricane-pms/hurricane-pms.tsx
@@ -1,18 +1,43 @@
+import { useEffect, useState } from "react";
 import { useDeviceType } from "@/lib/hooks";
 import styles from "./hurricane-pms.module.scss";
 import { DataGrid } from "@/components/data-grid";
-import { columnDefs } from "./hurricane-pms-config";
+import {
+  columnDefs,
+  defaultGridOptions,
+  managerDetailsColDefs,
+  profitColDefs,
+  lossColDefs,
+} from "./hurricane-pms-config";
+import {
+  managers,
+  manager_detail,
+  top_gainers,
+  top_losers,
+} from "./pms_mock_data";
 
 export const HurricanePms = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [managerId, setManagerId] = useState<string>('1');
+    const [managerDetails, setManagerDetails] = useState<any>(null);
     const deviceType = useDeviceType();
     const isMobile = deviceType !== 'desktop';
+
+    const handleOnRowClicked = (event: any) => {
+        const selectedRow = event.data;
+        setManagerId(selectedRow.id.toString());
+    };
+
+    useEffect(() => {
+      setManagerDetails(manager_detail[managerId as keyof typeof manager_detail] || []);
+    }, [managerId]);
 
   return (
     <div className={`${styles["hurricane-pms"]} scrollable-div gap-4`}>
       <section className="flex h-[600px] gap-4 flex-1">
-        <div className="w-[80%] flex gap-4">
+        <div className="w-[80%] flex gap-8">
 
-          <section className="w-[70%]">
+          <section className="w-[60%]">
             <DataGrid 
                 domLayout={'normal'}
                 height={isMobile ? 500 : '95%'}
@@ -20,14 +45,40 @@ export const HurricanePms = () => {
                 suppressStatusBar={true}
                 suppressFloatingFilter={false}
                 columnDefs={columnDefs}
+                rowData={managers}
+                gridOptions={{
+                  ...defaultGridOptions,
+                  onRowClicked: handleOnRowClicked,
+                }}
+                loading={isLoading}
             />
           </section>
-          <section className="flex flex-col gap-4">
-            <div>
-              <DataGrid />
+          <section className="w-[40%]">
+            <div className="h-1/2">
+                <DataGrid 
+                    domLayout={'normal'}
+                    height={isMobile ? 500 : '95%'}
+                    isSummaryGrid={false}
+                    suppressStatusBar={true}
+                    suppressFloatingFilter={false}
+                    columnDefs={profitColDefs}
+                    rowData={top_gainers}
+                    gridOptions={defaultGridOptions}
+                    loading={isLoading}
+                />
             </div>
-            <div>
-              <DataGrid />
+            <div className="h-1/2">
+                <DataGrid 
+                    domLayout={'normal'}
+                    height={isMobile ? 500 : '95%'}
+                    isSummaryGrid={false}
+                    suppressStatusBar={true}
+                    suppressFloatingFilter={false}
+                    columnDefs={lossColDefs}
+                    rowData={top_losers}
+                    gridOptions={defaultGridOptions}
+                    loading={isLoading}
+                />
             </div>
           </section>
 
@@ -41,7 +92,17 @@ export const HurricanePms = () => {
 
       <section className="flex h-[600px] gap-4">
         <div className="w-[80%]">
-          <DataGrid />
+            <DataGrid 
+            domLayout={'normal'}
+            height={isMobile ? 500 : '95%'}
+            isSummaryGrid={false}
+            suppressStatusBar={true}
+            suppressFloatingFilter={false}
+            columnDefs={managerDetailsColDefs}
+            rowData={managerDetails}
+            gridOptions={defaultGridOptions}
+            loading={isLoading}
+            />
         </div>
         <div>charts</div>
       </section>
