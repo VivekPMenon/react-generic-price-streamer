d1eb024b89c26f42168f81c875cb9e87cc8c7951
Rehan Azam <rehan786@aol.com>
2025-04-25 13:32:45 -0400

    add breaking news carousel on top

--- TransientAI.UI/transientai-ui/src/app/dashboard-generic/layout.tsx
+++ TransientAI.UI/transientai-ui/src/app/dashboard-generic/layout.tsx
@@ -19,6 +19,8 @@ import {BondNews} from "@/components/news/bond-news";
 import msalInstance from '../msal-config';
 import styles from './page.module.scss';
 import 'react-tooltip/dist/react-tooltip.css'
+import {EContentTypes} from "@/components/content-carousel/model";
+import {ContentCarousel} from "@/components/content-carousel/content-carousel";
 
 const MODE: Mode = Mode.SELL;
 
@@ -93,10 +95,15 @@ export default function DashboardLayout({
             }
           </div>
 
-          <div className={`${styles['middle-panel']} ${isMenuVisible && deviceType === 'mobile' ? styles['collapsed'] : ''}`}>
-            <DashboardTabs>
-              {children}
-            </DashboardTabs>
+          <div className={`${styles['middle-panel']}`}>
+            <div className={`${styles['middle-panel-top']} ${isMenuVisible && deviceType === 'mobile' ? styles['collapsed'] : ''}`}>
+              <ContentCarousel
+                  contentType={EContentTypes.NOTIFICATION}
+              />
+              <DashboardTabs>
+                {children}
+              </DashboardTabs>
+            </div>
 
             <div className={`${styles['middle-panel-bottom-widgets']}`}>
               <div className={styles.tradingActivity}>
